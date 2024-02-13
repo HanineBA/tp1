7f644747dadaa1c703a1c322a6ddf1e04a8ff027
tasnimmizaou <158618070+tasnimmizaou@users.noreply.github.com>
2024-02-13 13:34:09 +0100

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,31 @@
+
+/**
+ * cette fonction permet d'ajouter un element childElement portant les valeurs textuelles nameID et ValueID dans fatherID juste lorsqe le dom detecte l'evenement event dans le submitterID
+ * @param {*} submitterID : l'element responsable de l'ajout du childElement
+ * @param {*} eventt : l'evenement qui déclenchera l'ajout du childElement
+ * @param {*} fatherID :l'element ou on va ajouter le childElement
+ * @param {*} childElement : l'element à ajouter
+ * @param {*} nameID : text a mettre dans le childElement
+ * @param {*} valueID : texte a mettre dans le childElement
+ */
+function ajout(submitterID, eventt, fatherID, childElement, nameID, valueID) {
+    let submitter = document.querySelector(submitterID);
+    submitter.addEventListener(eventt, function(event) {
+        event.preventDefault();
+        
+    let father = document.querySelector(fatherID);
+    let child = document.createElement(childElement);
+    let name = document.querySelector(nameID).value;
+    let value = document.querySelector(valueID).value;
+       
+        if(value &&name){
+        child.textContent = `${name}:  ${value}`;
+        father.appendChild(child);
+        document.querySelector(nameID).value='';
+        document.querySelector(valueID).value='';
+        }
+        
+    });
+}
+
+ajout('button', 'click', '#list', 'li', '#name', '#content');
